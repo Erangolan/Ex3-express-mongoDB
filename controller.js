f62f1ec4a32ba0ed2aa1d53437bc8dedb2319086
Erangolan <37651948+Erangolan@users.noreply.github.com>
2020-01-13 19:48:52 +0200

    Update controller.js

--- controller.js
+++ controller.js
@@ -11,97 +11,61 @@ const options = {
     useUnifiedTopology: true,
 };
 
+mongoose.connect(url, options);
+const connection = mongoose.connection;
+connection.on('error', err => console.error('connection error: ', err));
+connection.once('open', () => console.log('connected to: ', connection.name));
 
 module.exports = {
-    getAllItems(req, res, next){
-        mongoose
-        .connect(url, options)
-        .then(async () => {
-            const result = await Item.find({});
-            if (result){
-                res.json(result);
-                console.log(result);
-            } else {
-                res.status(404).send('not found');
-            }
-        })
-        .catch(err => {
-            console.error('some error ocurred', err);
-            res.status(500).send(err.message);
-        })
+    async getAllItems(req, res, next){
+        const result = await Item.find({});
+        if (result){
+            res.json(result);
+            console.log(result);
+        } else {
+            res.status(404).send('not found');
+        }
     },
-    getItem(req, res, next){
-        mongoose
-        .connect(url, options)
-        .then(async () => {
-            const { id = null } = req.params;
-            const result = await Item.findOne({id});
-            if (result){
-                res.json(result);
-                console.log(result);
-            } else {
-                res.status(404).send('id not found');
-            }
-        })
-        .catch(err => {
-            console.error('some error ocurred', err);
-            res.status(500).send(err.message);
-        })
+    async getItem(req, res, next){
+        const { id = null } = req.params;
+        const result = await Item.findOne({id});
+        if (result){
+            res.json(result);
+            console.log(result);
+        } else {
+            res.status(404).send('id not found');
+        }
     },
-    editItem(req, res, next){
-        mongoose
-        .connect(url, options)
-        .then(async () => {
-            const { id = null } = req.params;
-            const { itemName = null, FirstPublished = null, itemCategory = null, reporter = null } = req.body;
-            const result = await Item.updateOne({ id }, { itemName, itemCategory, FirstPublished, reporter });
-            if (result) {
-                res.json(`item ${id} udated successfully!`);
-                console.log(`item ${id} udated successfully!`);
-            } else {
-                res.status(404).send('not found');
-            }
-        })
-        .catch(err => {
-            console.error('some error ocurred', err);
-            res.status(500).send(err.message);
-        })
+    async editItem(req, res, next){
+        const { id = null } = req.params;
+        const { itemName = null, FirstPublished = null, itemCategory = null, reporter = null } = req.body;
+        const result = await Item.updateOne({ id }, { itemName, itemCategory, FirstPublished, reporter });
+        if (result) {
+            res.json(`item ${id} udated successfully!`);
+            console.log(`item ${id} udated successfully!`);
+        } else {
+            res.status(404).send('not found');
+        }
     },
-    addItem(req, res, next){
-        mongoose
-        .connect(url, options)
-        .then(async () => {
-            const { id = null, itemName = null, firstPublished = null, itemCategory = null, reporter = null } = req.body;
-            const item = new Item({id, itemName, firstPublished, itemCategory, reporter});
-            const result = await item.save();
-            if (result){
-                res.json(result);
-                console.log(result);
-            } else {
-                res.status(404).send('not found');
-            }
-        })
-        .catch(err => {
-            console.error('some error ocurred', err);
-            res.status(500).send(err.message);
-        })
+    async addItem(req, res, next){
+        const { id = null, itemName = null, firstPublished = null, itemCategory = null, reporter = null } = req.body;
+        const item = new Item({id, itemName, firstPublished, itemCategory, reporter});
+        const result = await item.save();
+        if (result){
+            res.json(result);
+            console.log(result);
+        } else {
+            res.status(404).send('not found');
+        }
     },
-    removeItem(req, res, next){
-        mongoose
-        .connect(url, options)
-        .then(async () => {
-            const { id = null } = req.body;
-            const result = await Item.deleteOne({id});
-            if (result){
-                res.json(result);
-                console.log(result);
-            } else {
-                res.status(404).send('not found');
-            }
-        })
-        .catch(err => {
-            console.error('some error ocurred', err);
-            res.status(500).send(err.message);
-        })
+    async removeItem(req, res, next){
+        const { id = null } = req.params;
+        const result = await Item.deleteOne({id});
+        if (result){
+            res.json(result);
+            console.log(result);
+        } else {
+            res.status(404).send('not found');
+        }
     },
 };   
